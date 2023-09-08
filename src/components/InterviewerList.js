3e37a63f4e75752533f156c1339594d0f7a5e806
Manpreet Saini <mp_manpreet@hotmail.com>
2023-09-08 01:28:54 -0400

    fixed css

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
+import "../components/InterviewList.scss";
 
 export default function InterviewerList(props) {
   const setInterviewer = (id) => {
