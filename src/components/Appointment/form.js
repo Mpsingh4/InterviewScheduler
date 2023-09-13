77e1705cdac34941770592589860c94fc7aea356
Manpreet Saini <mp_manpreet@hotmail.com>
2023-09-13 03:43:27 -0400

    further implement props and state in app form

--- src/components/Appointment/form.js
+++ src/components/Appointment/form.js
@@ -5,10 +5,17 @@ import { useState } from "react";
 
 export default function Form(props) {
   const [studentName, setStudentName] = useState(props.student || "");
+  const [selectedInterviewer, setSelectedInterviewer] = useState(props.interviewer || null);
+
 
   const handleNameChange = (event) => {
     setStudentName(event.target.value);
   };
+
+  const handleInterviewerChange = (interviewerId) => {
+    setSelectedInterviewer(interviewerId);
+  };
+  
 
   const { interviewer, interviewers, onSave, onCancel } = props;
 
@@ -26,16 +33,17 @@ export default function Form(props) {
           />
         </form>
         <InterviewerList
-          interviewer={interviewer}
+           interviewer={selectedInterviewer}
           interviewers={interviewers}
-        />
+          onChange={handleInterviewerChange}
+          />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={onCancel}>
             Cancel
           </Button>
-          <Button confirm onClick={() => onSave(studentName, interviewer)}>
+          <Button confirm onClick={() => onSave(studentName, selectedInterviewer)}>
             Save
           </Button>
         </section>
@@ -43,34 +51,3 @@ export default function Form(props) {
     </main>
   );
 }
-
-
-// export default function Form(props) {
-//   return (
-//     <main className="appointment__card appointment__card--create">
-//   <section className="appointment__card-left">
-//     <form autoComplete="off">
-//       <input
-//         className="appointment__create-input text--semi-bold"
-//         name="name"
-//         type="text"
-//         placeholder="Enter Student Name"
-//         /*
-//           This must be a controlled component
-//           your code goes here
-//         */
-//       />
-//     </form>
-//     <InterviewerList 
-//       /* your code goes here */
-//     />
-//   </section>
-//   <section className="appointment__card-right">
-//     <section className="appointment__actions">
-//       <Button danger {/* your code goes here */}>Cancel</Button>
-//       <Button confirm {/* your code goes here */}>Save</Button>
-//     </section>
-//   </section>
-// </main>
-//   )
-// }
